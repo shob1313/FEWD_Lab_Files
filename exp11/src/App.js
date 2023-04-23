c4145bbfa475966f6c16c949ae5f2b8882db4da5
shob1313 <88094719+shob1313@users.noreply.github.com>
2023-04-23 10:47:20 +0530

    Update App.js

--- exp11/src/App.js
+++ exp11/src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 import Home from "./components/Home";
 import Aboutus from "./components/AboutUs";
 import Products from "./components/Products";
-import Contactus from "./components/Contactus";
+import ContactUs from "./components/ContactUs";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
           <Route exact path="/" element={<Home/>} />          
           <Route path="/aboutus" element ={<Aboutus/>} />
           <Route path="/products" element ={<Products/>} />
-          <Route path="/contactus" element ={<Contactus/>} />
+          <Route path="/contactUs" element ={<ContactUs/>} />
                                   
           
         </Routes>
